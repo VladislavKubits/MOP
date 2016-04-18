cc402b12b2ba4bf200c53d34e3c87b0f86a69140
kirillRomanenko <romanenko.kirill1995@gmail.com>
2016-04-18 19:36:55 +0300

    Тест

--- app/src/main/java/com/example/user/mop/MOP_Activity_Abiturient.java
+++ app/src/main/java/com/example/user/mop/MOP_Activity_Abiturient.java
@@ -8,5 +8,6 @@ public class MOP_Activity_Abiturient extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mop_activity_abiturient);
+        // test
     }
 }
