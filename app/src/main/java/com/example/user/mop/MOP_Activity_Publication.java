ec2e6e5be504d380a3fe015b7d982b22d3049292
kirillRomanenko <romanenko.kirill1995@gmail.com>
2016-05-16 18:50:06 +0300

    удалил спиннеры

--- app/src/main/java/com/example/user/mop/MOP_Activity_Publication.java
+++ app/src/main/java/com/example/user/mop/MOP_Activity_Publication.java
@@ -4,69 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+
 
 public class MOP_Activity_Publication extends AppCompatActivity {
 
-    String[] menu = {"Отправить новость в соцсети", "Новости", "Расписание", "О кафедре", "Авторизация", "Абитуриент"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mop_activity_publication);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, menu);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner4);
-        spinner.setAdapter(adapter);
-
-        spinner.setPrompt("Главное меню");
-
-        spinner.setSelection(0);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                                              @Override
-                                              public void onItemSelected(AdapterView<?> parent,
-                                                                         View view, int position, long id) {
-
-                                                  if (id == 1) {
-                                                      Intent i = new Intent(MOP_Activity_Publication.this,
-                                                              MOP_Activity_MOP_NEWS.class);
-                                                      startActivity(i);
-                                                  }
-
-                                                  if (id == 2) {
-                                                      Intent i = new Intent(MOP_Activity_Publication.this,
-                                                              MOP_Activity_Timetable.class);
-                                                      startActivity(i);
-                                                  }
-
-                                                  if (id == 3) {
-                                                      Intent i = new Intent(MOP_Activity_Publication.this,
-                                                              MOP_Activity_History_of_MOP_AVM.class);
-                                                      startActivity(i);
-                                                  }
-
-                                                  if (id == 4) {
-                                                      Intent i = new Intent(MOP_Activity_Publication.this,
-                                                              MOP_Activity_Autoresation.class);
-                                                      startActivity(i);
-                                                  }
-
-                                                  if (id == 5) {
-                                                      Intent i = new Intent(MOP_Activity_Publication.this,
-                                                              MOP_Activity_Abiturient.class);
-                                                      startActivity(i);
-                                                  }
-                                              }
-
-                                              @Override
-                                              public void onNothingSelected(AdapterView<?> parent) {
-                                              }
-                                          }
-        );
     }
 }
