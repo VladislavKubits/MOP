9558d1875efe0bd018ff28ec46185a97ece498dd
kirillRomanenko <romanenko.kirill1995@gmail.com>
2016-05-20 19:35:00 +0300

    ActionBar в новостях

--- app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
+++ app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
@@ -1,12 +1,15 @@
 package com.example.user.mop;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MOP_Activity_MOP_NEWS extends AppCompatActivity {
+    private Toolbar toolbar;
 
 
 
@@ -15,5 +18,42 @@ public class MOP_Activity_MOP_NEWS extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mop_activity_mop_news);
 
+        toolbar=(Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+
+        if (id==R.id.news){
+            return true;
+        }
+        if (id==R.id.schedule){
+            return true;
+        }
+        if (id==R.id.About_the_department){
+            return true;
+        }
+        if (id==R.id.Send_news_to_social_networks){
+            return true;
+        }
+        if (id==R.id.authorization){
+            return true;
+        }
+        if (id==R.id.Enrollee){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
