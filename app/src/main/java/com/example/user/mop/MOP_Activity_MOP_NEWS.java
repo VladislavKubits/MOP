7cdcc2eb34b70bc4e511aefd496fed3b934da5b2
VladislavKubits <user@user-PC.mopevm.tsure.ru>
2016-06-02 12:31:39 +0300

    Тулбар в активити Новости готов

--- app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
+++ app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
@@ -1,6 +1,7 @@
 package com.example.user.mop;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,21 +35,33 @@ public class MOP_Activity_MOP_NEWS extends AppCompatActivity {
         int id=item.getItemId();
 
         if (id==R.id.news){
+            Intent intent = new Intent(this, MOP_Activity_MOP_NEWS.class);
+            startActivity(intent);
             return true;
         }
         if (id==R.id.schedule){
+            Intent intent = new Intent(this, MOP_Activity_Timetable.class);
+            startActivity(intent);
             return true;
         }
         if (id==R.id.About_the_department){
+            Intent intent = new Intent(this, MOP_Activity_History_of_MOP_AVM.class);
+            startActivity(intent);
             return true;
         }
         if (id==R.id.Send_news_to_social_networks){
+            Intent intent = new Intent(this, MOP_Activity_Publication.class);
+            startActivity(intent);
             return true;
         }
         if (id==R.id.authorization){
+            Intent intent = new Intent(this, MOP_Activity_Autoresation.class);
+            startActivity(intent);
             return true;
         }
         if (id==R.id.Enrollee){
+            Intent intent = new Intent(this, MOP_Activity_Abiturient.class);
+            startActivity(intent);
             return true;
         }
 
