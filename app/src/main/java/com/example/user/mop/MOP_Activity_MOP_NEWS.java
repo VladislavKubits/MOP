ad0828aa99f62a07c94732b65e256b03374b92f7
kirillRomanenko <romanenko.kirill1995@gmail.com>
2016-07-27 02:10:15 +0300

    посты со страницы вк
    пока выводит в консоль (надо ещё поработать над читабельным видом)

--- app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
+++ app/src/main/java/com/example/user/mop/MOP_Activity_MOP_NEWS.java
@@ -8,62 +8,75 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApiConst;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.methods.VKApiGroups;
+import com.vk.sdk.api.methods.VKApiUsers;
+import com.vk.sdk.api.methods.VKApiWall;
+import com.vk.sdk.api.model.VKList;
+
+import org.json.JSONException;
 
 
 public class MOP_Activity_MOP_NEWS extends AppCompatActivity {
     private Toolbar toolbar;
 
-    private String[] scope=new String[] {VKScope.WALL};
+    private String[] scope = new String[]{VKScope.WALL};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mop_activity_mop_news);
-        VKSdk.login(this,scope);
+        VKSdk.login(this, scope);
 
-        toolbar=(Toolbar) findViewById(R.id.my_toolbar);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id=item.getItemId();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
-        if (id==R.id.news){
+        if (id == R.id.news) {
             Intent intent = new Intent(this, MOP_Activity_MOP_NEWS.class);
             startActivity(intent);
             return true;
         }
-        if (id==R.id.schedule){
+        if (id == R.id.schedule) {
             Intent intent = new Intent(this, MOP_Activity_Timetable.class);
             startActivity(intent);
             return true;
         }
-        if (id==R.id.About_the_department){
+        if (id == R.id.About_the_department) {
             Intent intent = new Intent(this, MOP_Activity_History_of_MOP_AVM.class);
             startActivity(intent);
             return true;
         }
-        if (id==R.id.Send_news_to_social_networks){
+        if (id == R.id.Send_news_to_social_networks) {
             Intent intent = new Intent(this, MOP_Activity_Publication.class);
             startActivity(intent);
             return true;
         }
-        if (id==R.id.authorization){
+        if (id == R.id.authorization) {
             Intent intent = new Intent(this, MOP_Activity_Autoresation.class);
             startActivity(intent);
             return true;
         }
-        if (id==R.id.Enrollee){
+        if (id == R.id.Enrollee) {
             Intent intent = new Intent(this, MOP_Activity_Abiturient.class);
             startActivity(intent);
             return true;
@@ -71,6 +84,45 @@ public class MOP_Activity_MOP_NEWS extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+                final VKRequest VKRequest = new VKApiUsers().get(VKParameters.from("user_ids", "software.engineering"));
+                VKRequest.executeWithListener(new VKRequest.VKRequestListener() {
+                    @Override
+                    public void onComplete(VKResponse response) {
+                        super.onComplete(response);
+                        VKList vkList = (VKList) response.parsedModel;
+                        try {
+                            VKRequest VKRequest1 = new VKApiWall().get(VKParameters.from(VKApiConst.OWNER_ID, vkList.get(0).fields.getInt("id"), VKApiConst.COUNT, 100));
+                            VKRequest1.executeWithListener(new VKRequest.VKRequestListener() {
+                                @Override
+                                public void onComplete(VKResponse response) {
+                                    super.onComplete(response);
+
+                                    System.out.println(response.responseString);
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
